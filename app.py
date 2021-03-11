e77703e8c87597ebfe2a0b9357d786fc54ffd9dd
David Zemel <zemel96@gmail.com>
2021-03-10 19:20:34 -0500

    working on user edits

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, render_template
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -12,11 +12,67 @@ db.create_all()
 # app.config['SECRET_KEY'] = "SECRET!"
 # debug = DebugToolbarExtension(app)
 
+
 @app.route('/')
+def home_redirect():
+
+    return redirect('/users')
+
+
+@app.route('/users')
 def home():
+
+    users = User.query.all()
     return render_template(
-        'base.html'
+        'home.html',
+        users=users
     )
-    #show users, list of users, button to add users
+    # show users, list of users, button to add users
 
 
+@app.route('/users/new')
+def add_user():
+
+    return render_template(
+        'create_user.html'
+    )
+
+
+@app.route('/users/new', methods=["POST"])
+def create_user():
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    link = request.form['image_url']
+
+    user = User(first_name=first_name, last_name=last_name, image_url=link)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>')
+def show_user(user_id):
+
+    user = User.query.get_or_404(user_id)
+    return render_template("detail.html", user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def edit_user():
+    first_name = request.form['edit_first_name']
+    last_name = request.form['edit_last_name']
+    link = request.form['edit_image_url']
+
+    user = User(first_name=first_name, last_name=last_name, image_url=link)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/{{user.id}}/edit')
+def link_edit():
+
+    return render_template("edit_user.html")
--- models.py
+++ models.py
@@ -2,24 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
     """Connect to database."""
     db.app = app
     db.init_app(app)
 
+
 class User(db.Model):
+    """ Users """
 
     __tablename__ = "users"
 
     id = db.Column(db.Integer,
-                primary_key =True,
-                autoincrement=True)
+                   primary_key=True,
+                   autoincrement=True)
     first_name = db.Column(db.String(50),
-                nullable=False,
-                unique=True)
+                           nullable=False,
+                           unique=True)
     last_name = db.Column(db.String(50),
-                nullable=False,
-                unique=True)
-    image_url = db.Column(db.String(100),
-                nullable=False,
-                unique=True)
+                          nullable=False,
+                          unique=True)
+    image_url = db.Column(db.Text,
+                          nullable=True,
+                          unique=True)
